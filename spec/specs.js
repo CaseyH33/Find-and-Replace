dd517f6847b0310bf9a044defde12212057c9c12
Casey Heitz <heitz.casey@gmail.com>
2015-09-11 09:34:40 -0700

    Add passing test for changing multiple instances of a word

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('findAndReplace', function() {
     it("will change a certain word in the string to a new word", function() {
         expect(findAndReplace("Hello world", "world", "universe")).to.eql(["Hello", "universe"]);
     });
+
+    it("will change multiple instances of the same word to a new word", function() {
+        expect(findAndReplace("Hello world world", "world", "universe")).to.eql(["Hello", "universe", "universe"]);
+    });
 });
