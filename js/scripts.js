0a247348adedace4f8b514a4b39156b85885a06b
Casey Heitz <heitz.casey@gmail.com>
2015-09-11 09:49:11 -0700

    Add html page and jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,18 @@ var findAndReplace = function(string, find_word, change_word) {
         }
     }
     return input_array;
-}
+};
+
+$(function() {
+  $("form#find_replace").submit(function(event) {
+    var input_string = $("input#input_string").val();
+    var find_word = $("input#find_word").val();
+    var change_word = $("input#change_word").val();
+    var result = findAndReplace(input_string, find_word, change_word);
+
+    $(".output").empty();
+    $(".output").text(result.join(" "));
+    $("#result").show();
+    event.preventDefault();
+  });
+});
